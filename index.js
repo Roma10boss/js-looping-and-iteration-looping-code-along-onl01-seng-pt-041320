70a94a0d0648640b9e32c6259d40fc94261f5745
Romario Gustave <romagustave10@gmail.com>
2020-11-01 16:19:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 // Code your solutions in this file
-const cards= ["Ada", "Brendan", "Ali"];
-
-Function wrapCards(cards){
-for (let i=0 ; i< cards.length;i++){
-  console.log('Thank you ${cards[i] for the birthday wishes);
-}
-return cards;
-}
